90ce8447b98619d5ca603a7196811cceb0f977b0
Aurorn <aurornispro@gmail.com>
2024-11-28 18:32:51 -0500

    Second Deploying Commit

--- README.md
+++
@@ -1,1 +0,0 @@
-This is an under construction for the Odin Project - Homepage.
--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-
-export default [
-    { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
-    pluginJs.configs.recommended,
-    { ignores: ['node_modules/', 'dist/', 'src/test/'] },
-]
